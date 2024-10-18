35ccd3a4f44c9c3ca7ee8322912cb0f8d40a46a4
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2024-10-18 03:26:04 -0500

    add breed

---
+++ Backend/src/controllers/breed.controller.js
@@ -0,0 +1,12 @@
+import { Breed } from '../models/breed.js';
+
+export const getBreeds = async (req, res) => {
+    try {
+        const breeds = await Breed.findAll();
+        res.json(breeds);
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ msg: "Error al obtener razas", error });
+    }
+};
--- Backend/src/routes/animal.routes.js
+++ Backend/src/routes/animal.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { createAnimal, getAnimal, getAnimals, deleteAnimal, updateAnimal } from '../controllers/animal.controller.js'
-
+import { getBreeds } from '../controllers/breed.controller.js'
 const router = Router()
 
 router.get('/', getAnimals)
@@ -9,5 +9,7 @@ router.post('/crear', createAnimal)
 router.delete('/:id', deleteAnimal)
 router.get('/:id', getAnimal)
 
-router.put('/:id', updateAnimal)
+router.put('/:id', updateAnimal) 
+
+router.get('/breeds', getBreeds)
 export default router
